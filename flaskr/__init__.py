e4f68471faaaf152bb54e112067c0d4e7f414a9c
micahoriaso <micahoriaso@gmail.com>
2018-06-20 00:02:11 +0300

    [Feature #158459164] Create Rest API endpoint for ride offers

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask
 
-# Create the appliction instance
-app = Flask(__name__, template_folder='../UI')
+def create_app(test_config=None):
+    # Create an instance of the flask application
+    app = Flask(__name__)
 
-# Create a URL route in our application for "/""
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.update(test_config)
+    
+    # Apply blueprints to the app
+    from flaskr import rides
+    app.register_blueprint(rides.bp)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+    if __name__ == '__main__':
+        app.run(debug=True)
+        
+    return app
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
