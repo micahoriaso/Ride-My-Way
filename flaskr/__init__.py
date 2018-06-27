ee867f6cb3c5534f15d0f7ac360eb65108058c1f
micahoriaso <micahoriaso@gmail.com>
2018-06-27 18:48:27 +0300

    [Chore #158626135] Update database connection

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -10,22 +10,10 @@ from resourcesV2.requests import requests_v2_bp
 from resourcesV2.rides import rides_v2_bp
 from resourcesV2.users import users_v2_bp
 
-from flaskr.db import create_tables
-
-
-def create_app(test_config=None):
+def create_app(configObject):
     # Create an instance of the flask application
     app = Flask(__name__)
-    app.config.from_mapping(
-        # a default secret that should be overridden by instance config
-        SECRET_KEY='my-very-long-secret-key',
-    )
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.update(test_config)
+    app.config.from_object(configObject)
 
     app.register_blueprint(requests_v1_bp)
     app.register_blueprint(rides_v1_bp)
@@ -37,7 +25,6 @@ def create_app(test_config=None):
 
     if __name__ == '__main__':
         app.run(debug=True)
-
         
     return app
 
