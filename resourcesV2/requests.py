dce1991ad3f5f5672e2f5da4f5b3b1adabc8b0ca
micahoriaso <micahoriaso@gmail.com>
2018-06-27 23:23:27 +0300

    [Feature #158667015] Add delete ride request  API

--- resourcesV2/requests.py
+++ resourcesV2/requests.py
@@ -55,7 +55,39 @@ class RequestListResource(Resource):
         return results
 
 class RequestResource(Resource):
-    pass
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'request_status', type=str, location='json', default='Pending'
+        )
+
+        self.connection = connectDB()
+        self.cursor = self.connection.cursor(
+            cursor_factory=psycopg2.extras.DictCursor)
+        super(RequestResource, self).__init__()
+
+    # DELETE method for deleting a ride request
+    def delete(self, ride_id, request_id):
+        self.abort_if_ride_request_doesnt_exist(request_id)
+        try:
+            self.cursor.execute('DELETE FROM ride_request WHERE id = %s ;',
+                                ([request_id]))
+            self.connection.commit()
+        except (Exception, psycopg2.DatabaseError) as error:
+            self.connection.rollback()
+            return {'status': 'failed', 'data': error}, 200
+        return {'status': 'success', 'data': 'Ride request successfully deleted'}, 200
+
+    def abort_if_ride_request_doesnt_exist(self, request_id):
+        try:
+            self.cursor.execute('SELECT * FROM ride_request WHERE id = %s ;',
+                                ([request_id]))
+        except (Exception, psycopg2.DatabaseError) as error:
+            self.connection.rollback()
+            return {'status': 'failed', 'data': error}, 500
+        results = self.cursor.fetchone()
+        if results is None:
+            abort(404, message='The ride request with id {} does not exist'.format(request_id))
 
 
 requests_v2_bp = Blueprint('resourcesV2.requests', __name__)
