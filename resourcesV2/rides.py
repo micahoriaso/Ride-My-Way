8cdeb0ad4636bf41a4fae8f1b9f9332cb0b6a898
micahoriaso <micahoriaso@gmail.com>
2018-06-27 21:19:31 +0300

    [Feature #158667162] Add view ride details API

--- resourcesV2/rides.py
+++ resourcesV2/rides.py
@@ -86,7 +86,60 @@ class RideListResource(Resource):
 
 
 class RideResource(Resource):
-    pass
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'dropoff', type=str, required=True, help='Please enter dropoff', location='json'
+        )
+        self.reqparse.add_argument(
+            'pickup', type=str, required=True, help='Please enter pickup', location='json'
+        )
+        self.reqparse.add_argument(
+            'time', type=str, required=True, help='Please enter time', location='json'
+        )
+        self.reqparse.add_argument(
+            'price', type=float, required=True, help='Please enter price', location='json'
+        )
+        self.reqparse.add_argument(
+            'date', type=str, required=True, help='Please enter the date', location='json'
+        )
+        self.reqparse.add_argument(
+            'capacity', type=int, required=True, help='Please enter the vehicle capacity', location='json'
+        )
+        self.reqparse.add_argument(
+            'available_seats', type=int, required=True, help='Please enter the available seats', location='json'
+        )
+        self.reqparse.add_argument(
+            'driver_id', type=int, required=True, help='Please enter the driver', location='json'
+        )
+        self.reqparse.add_argument(
+            'registration', type=str, required=True, help='Please enter the registration', location='json'
+        )
+        self.reqparse.add_argument(
+            'status', type=str, required=False, help='Please enter the ride status', default='In Offer', location='json'
+        )
+
+        self.connection = connectDB()
+        self.cursor = self.connection.cursor(
+            cursor_factory=psycopg2.extras.DictCursor)
+        super(RideResource, self).__init__()
+        
+    # GET method for a ride request
+    def get(self, ride_id):
+        request = self.abort_if_ride_doesnt_exist(ride_id)
+        return {'data': request}
+
+    def abort_if_ride_doesnt_exist(self, ride_id):
+        try:
+            self.cursor.execute('SELECT * FROM ride WHERE id = %s ;',
+                                ([ride_id]))
+        except (Exception, psycopg2.DatabaseError) as error:
+            self.connection.rollback()
+            return {'status': 'failed', 'data': error}, 500
+        results = self.cursor.fetchone()
+        if results is None:
+            abort(404, message='The ride with id {} does not exist'.format(ride_id))
+        return results
 
 
 rides_v2_bp = Blueprint('resourcesV2.rides', __name__)
