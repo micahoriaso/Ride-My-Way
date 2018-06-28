2053d0853d4d9861b1d9e0d400d6456b43bfc50e
micahoriaso <micahoriaso@gmail.com>
2018-06-28 10:14:31 +0300

    [Feature #158667423] Add delete user API

--- resourcesV2/users.py
+++ resourcesV2/users.py
@@ -1,10 +1,14 @@
+import psycopg2
+import psycopg2.extras
+
 from flask import Blueprint
 
 from flask_restful import Resource, reqparse, fields, marshal, abort, Api
 
 from werkzeug.security import generate_password_hash, check_password_hash
 
-users = []
+from flaskr.db import connectDB
+
 
 class UserListResource(Resource):
     pass
@@ -15,24 +19,69 @@ class LoginResource(Resource):
 
 
 class UserResource(Resource):
-    pass
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'firstname', type=str, location='json'
+        )
+        self.reqparse.add_argument(
+            'lastname', type=str, location='json'
+        )
+        self.reqparse.add_argument(
+            'email', type=str, location='json'
+        )
+        self.reqparse.add_argument(
+            'car_registration', location='json'
+        )
+        self.reqparse.add_argument(
+            'password', type=str, location='json'
+        )
+
+        self.connection = connectDB()
+        self.cursor = self.connection.cursor(
+            cursor_factory=psycopg2.extras.DictCursor)
+        super(UserResource, self).__init__()
+
+    # DELETE method for deleting a user
+    def delete(self, user_id):
+        self.abort_if_user_doesnt_exist(user_id)
+        try:
+            self.cursor.execute('DELETE FROM app_user WHERE id = %s ;',
+                                ([user_id]))
+            self.connection.commit()
+        except (Exception, psycopg2.DatabaseError) as error:
+            self.connection.rollback()
+            return {'status': 'failed', 'data': error}, 200
+        return {'status': 'success', 'data': 'User successfully deleted'}, 200
+
+
+    def abort_if_user_doesnt_exist(self, user_id):
+        try:
+            self.cursor.execute('SELECT * FROM app_user WHERE id = %s ;',
+                                ([user_id]))
+        except (Exception, psycopg2.DatabaseError) as error:
+            self.connection.rollback()
+            return {'status': 'failed', 'data': error}, 500
+        results = self.cursor.fetchone()
+        if results is None:
+            abort(404, message='The user with id {} does not exist'.format(user_id))
+        return results
 
 
 users_v2_bp = Blueprint('resourcesV2.users', __name__)
 api = Api(users_v2_bp)
 api.add_resource(
-    UserListResource, 
+    UserListResource,
     '/api/v2/auth/signup',
     '/api/v2/auth/signup/'
-    )
+)
 api.add_resource(
-    LoginResource, 
+    LoginResource,
     '/api/v2/auth/login',
     '/api/v2/auth/login'
-    )
+)
 api.add_resource(
-    UserResource, 
+    UserResource,
     '/api/v2/users/<user_id>',
     '/api/v2/users/<user_id>'
-    )
-
+)
